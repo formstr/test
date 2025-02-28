19e2a04da17b0901a2b99902c72ce785ef9ecee2
formstr <rob.formstone@gmail.com>
2025-02-28 15:16:19 +0000

    Update auth.tf

--- auth.tf
+++ auth.tf
@@ -2,9 +2,4 @@ provider "azurerm" {
   # Configuration options
   features {}
 
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-
 }
